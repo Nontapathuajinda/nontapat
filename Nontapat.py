abb4da2cd7180d52ccfb3b78beb2e743b27211a7
Nontapathuajinda <nontapatpoom585@gmail.com>
2025-04-27 15:43:19 +0700

    Add files via upload

---
+++ Nontapat.py
@@ -0,0 +1,55 @@
+import streamlit as st
+st.title("BMI Calculation")
+
+sex = st.radio("เพศ",("ชาย","หญิง"), horizontal=True)
+kg=st.number_input("น้ำหนัก(kg.)",value=55.0,min_value=10.0,max_value=200.0,
+                   step=0.5)
+cm=st.number_input("ส่วนสูง(cm.)",value=175,min_value=1,max_value=200,
+                   step=1)
+if st.button("CALCULATE"):
+    bmi=kg/(cm/100)**2
+    st.write(bmi)
+    if sex=="ชาย":
+        if bmi < 19:
+            st.info("ผอมไป")
+            st.warning("เส่ียงโรคขาดสารอาหาร")
+            st.image("1.png")
+        elif bmi <25:
+            st.success("ปกติ")
+            st.warning("โอกาสมีโรคแทรกซ้อนน้อย")
+            st.image("2.png")
+        elif bmi < 30:
+            st.warning("เกิน")
+            st.warning("ภาวะน้ำหนักเกินระยะเริ่มต้น")
+            st.image("3.png")
+        elif bmi >= 30: 
+            st.error("อ้วนมาก")
+            st.warning("ภาวะน้ำหนักเกินมากโรคอ้วน")
+            st.image("4.png")
+    else:
+        if bmi < 18:
+            st.info("ผอมไป")
+            st.warning("เส่ียงโรคขาดสารอาหาร")
+            st.image("wo1.png")
+        elif bmi <24:
+            st.success("ปกติ")
+            st.warning("โอกาสมีโรคแทรกซ้อนน้อย")
+            st.image("wo2.png")
+        elif bmi < 29:
+            st.warning("เกิน")
+            st.warning("ภาวะน้ำหนักเกินระยะเริ่มต้น")
+            st.image("wo3.png")
+        elif bmi >= 30: 
+            st.error("อ้วนมาก")
+            st.warning("ภาวะน้ำหนักเกินมากโรคอ้วน")
+            st.image("wo4.png")
+bg1="""
+<style>
+.stApp {
+    background-color: #8CBEDF;
+    color: #ffffff;
+}
+</style>
+"""
+
+st.html(bg1)
